56cadfa954641fc5eeb3127a31ac0ab839720ea4
ak-dvlp <Aleksandr-Karpenko-86@yandex.ru>
2026-07-30 22:11:48 +0300

    документация: добавлены компоненты BaseLoader и BaseClear; доработан компонент Example1UseValidateSeq

--- docs/composables/use-validate-seq.md
+++ docs/composables/use-validate-seq.md
@@ -5,23 +5,35 @@ import { data } from '../composables.data.ts'
 import { ref } from 'vue'
 
 const error1 = ref(false)
+const inputModel = ref('admin')
 </script>
 
 <ComposableTable class="mt-3" :data="data.useValidateSeq"/>
 
 ## Примеры
 
-Каждый пример ниже содержит одно синхронное правило проверяющее поле на пустоту и одно асинхронное правило имитирующее запрос на сервер длительностью 
+::: info Информация
+Каждый пример ниже использует массив правил `rules` имеющий одно синхронное правило (проверяет поле ввода на наличие непустого значения) и одно асинхронное правило (имитирует запрос на сервер). Массив правил может быть передан через параметр компонента, но в примерах для удобства отображения он объявлен непосредственно в блоке `<script>` примеров.
+:::
+
+### Синхронная и асинхронная проверка поля через вызов функции validate
 
 <details>
-<summary class="select-none cursor-pointer">Example1UseValidateSeq</summary>
+<summary class="select-none cursor-pointer">Код</summary>
 
-<<< @/../examples/src/components/Example1UseValidateSeq.vue#example-1-use-validate-seq {vue}
+<<< @/../examples/src/components/Example1UseValidateSeq.vue#example-1-use-validate-seq {28vue}
 
 </details>
 
-<ExampleContainer>
-  <Example1UseValidateSeq label="Логин" placeholder="Обязательное поле" />
+В примере ниже асинхронное правило возвращает ошибку, в том случае если поле содержит значение `'admin'`.
+
+<ExampleContainer #default="{ modelValue, updateModelValue }" v-model="inputModel">
+<Example1UseValidateSeq
+label="Логин"
+:model-value="modelValue"
+placeholder="Обязательное поле"
+@update:model-value="updateModelValue"
+/>
 </ExampleContainer>
 
 <!-- <ExampleContainer> <div>
--- examples/src/components/Example1UseValidateSeq.vue
+++ examples/src/components/Example1UseValidateSeq.vue
@@ -3,6 +3,8 @@
 import type { ExampleState } from '../../../docs/.vitepress/theme/components/ExampleContainer.vue'
 import { useValidateSeq } from '../../../src/composables/useValidateSeq'
 import type { BaseInputModel } from '../../../src/index'
+import BaseClear from './base/BaseClear.vue'
+import BaseLoader from './base/BaseLoader.vue'
 import ExampleButton from './ExampleButton.vue'
 
 const model = defineModel<BaseInputModel>()
@@ -19,31 +21,45 @@ const isRequiredSync = (value: string) => !!value || 'Поле обязател�
 
 /** Асинхронная правило */
 const checkRemoteAsync = async (value: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   return value !== 'admin' || 'Этот логин уже занят'
 }
 
-const { errExist, errMessage, reset, uid, validate } = useValidateSeq(model, {
-  rules: [isRequiredSync, checkRemoteAsync],
-})
+const rules = [isRequiredSync, checkRemoteAsync]
+
+const { errExist, errMessage, isValidating, reset, uid, validate } = useValidateSeq(model, { rules })
 </script>
 
 <template>
   <div class="flex w-full flex-col items-end gap-y-3">
     <div class="flex w-full flex-col gap-y-1">
       <label class="text-xs" :for="uid">{{ label }}</label>
-      <BaseInput
-        :id="uid"
-        v-model="model"
-        class="bg-my-body-background selection:bg-my-label selection:text-my-body-background w-full rounded-lg px-4 py-2.5 transition-all duration-150 outline-none"
-        :class="errExist ? 'text-my-error ring' : 'text-my-label hover:ring focus:ring'"
-        :placeholder
-      />
-      <div class="text-my-error text-xs">{{ errMessage }}</div>
+
+      <div
+        class="bg-my-body-background flex items-center gap-x-2 rounded-lg pe-3 transition-all duration-150 outline-none"
+        :class="errExist ? 'text-my-error ring' : 'text-my-label focus-within:ring hover:ring'"
+      >
+        <BaseInput
+          :id="uid"
+          v-model="model"
+          class="selection:text-my-body-background w-full py-2.5 ps-4 text-ellipsis outline-none"
+          :class="errExist ? 'selection:bg-my-error selection:' : 'selection:bg-my-label'"
+          :placeholder
+        />
+        <BaseLoader v-if="isValidating" :class="errExist ? 'text-my-error' : 'text-my-label'" />
+        <BaseClear
+          v-if="model && model !== ''"
+          :class="errExist ? 'fill-my-error' : 'fill-my-label'"
+          @click="model = ''"
+        />
+      </div>
+
+      <div v-if="errExist" class="text-my-error text-xs">{{ errMessage }}</div>
+      <div v-else class="h-4"></div>
     </div>
 
     <div class="flex flex-wrap gap-3">
-      <ExampleButton kind="success" label="Сбросить" @click="reset" />
+      <ExampleButton kind="success" label="Сбросить ошибку" @click="reset" />
       <ExampleButton label="Проверить вручную" @click="validate" />
     </div>
   </div>
